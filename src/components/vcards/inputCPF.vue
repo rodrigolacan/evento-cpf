df4b1dd26d061facc034b5fb666557696884454a
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-02-19 10:21:36 -0400

    refact: ajustando component que busca os dados apartir do cpf

--- src/components/vcards/inputCPF.vue
+++ src/components/vcards/inputCPF.vue
@@ -13,19 +13,72 @@
       />
     </div>
   </div>
+
+  <!-- Modal para exibir os resultados -->
+  <q-dialog v-model="showModal">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Resultado da busca</div>
+      </q-card-section>
+      <q-card-section>
+        <pre>{{ responseData }}</pre>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn label="Fechar" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, getCurrentInstance } from 'vue';
+import { ref, watch, getCurrentInstance } from 'vue'
 
-const instance = getCurrentInstance();
-const masks = instance?.appContext.config.globalProperties?.$masks;
+const instance = getCurrentInstance()
+const masks = instance?.appContext.config.globalProperties?.$masks
+const axios = instance?.appContext.config.globalProperties?.$api
+const isLoading = ref<boolean>(false)
+const cpf = ref<string>('')
+const showModal = ref<boolean>(false)
+const responseData = ref<Record<string, unknown> | null>(null)
+let timeoutId: ReturnType<typeof setTimeout> | null = null
 
-const cpf = ref<string>('');
+const buscarDadosCPF = async (newCpf: string) => {
+  isLoading.value = true
+  try {
+    const cpfSemMascara = newCpf.replace(/\D/g, '')
+    console.log(cpfSemMascara);
+    const response = await axios.get('SelecionarPessoaFisica', {
+      params: {
+        CgcCpf: cpfSemMascara,
+      },
+      headers:{
+        'x-req': import.meta.env.VITE_API_KEY
+      }
+    })
+    responseData.value = response.data
+    showModal.value = true
+  } catch (error) {
+    console.error('Erro na requisição:', error)
+    instance?.appContext.config.globalProperties?.$q.notify({
+      type: 'negative',
+      message: 'Erro ao buscar os dados do CPF.',
+    })
+  } finally {
+    isLoading.value = false
+  }
+}
 
 watch(cpf, (newCpf) => {
   if (masks) {
-    cpf.value = masks.cpf(newCpf);
+    cpf.value = masks.cpf(newCpf)
   }
-});
+
+  if (timeoutId) {
+    clearTimeout(timeoutId)
+  }
+
+  timeoutId = setTimeout(() => {
+    void buscarDadosCPF(newCpf);
+  }, 2000)
+})
 </script>
