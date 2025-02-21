02eda0a4f068cd89607306909e38e5c18888d947
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-02-21 09:30:05 -0400

    refact: mudando a url de acordo com o ambiente

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -14,7 +14,12 @@ declare module 'vue' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: import.meta.env.VITE_ROOT_URL_PROXY });
+const api = axios.create({ 
+  baseURL: window.location.hostname === 'localhost' 
+    ? 'http://localhost:8000/api' 
+    : '/api'
+});
+
 
 export default defineBoot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
